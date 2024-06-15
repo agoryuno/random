5354f4d471a43ae399c23e9b610b1a0cb03000d0
Alexander Goryunov <alex.goryunov@gmail.com>
2024-06-15 17:36:30 +0300

    code cleanup

--- test.c
+++ test.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <math.h>
 #include <time.h>
 #include <assert.h>
 
-//#include "tree.h"
-//#include "gw_knuth.h"
 #include "sample_wor.h"
 
 int main() {
     srand(time(NULL));
-    //double random_number = (double)rand() / RAND_MAX;  // Generate a random number between 0 and 1
 
-    //int vals[] = {0,1,2,3,4,5,6,7,8,9};
     double probs[10] = {0.1, 0.05, 0.05, 0.1, 0.2, 0.08, 0.02, 0.1, 0.15, 0.15};
 
     double sum_prob = 0.0;
@@ -24,7 +18,7 @@ int main() {
     SamplerState* sampler = get_sampler(probs, 10);
 
     #ifdef DEBUG_PRINT
-    printf("main(): received sampler, root label = %d\n", sampler->root->label);
+        printf("main(): received sampler, root label = %d\n", sampler->root->label);
     #endif
 
     for (int i=0; i<=11; i++) {
